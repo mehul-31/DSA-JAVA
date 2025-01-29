206c573066ec9fad1d5ab26eff6b061cab8a7486
Mehul Nandaniya <220130107062@gecg28.ac.in>
2025-01-29 15:55:39 +0530

    DSA Practice Done

--- src/Pattern19.java
+++ src/Pattern19.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
-public class Pattern18 {
+public class Pattern19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int i = 1;
         while (i<=n){
             int j = 1;
-            int space = 1;
-            while (space<=n-i){
-                System.out.print(" ");
-                space++;
-            }
-            while (j<=i){
+            //part 1 numbers
+            while (j<= n - i + 1){
                 System.out.print(j);
                 j++;
             }
-            j = i - 1;
-            while (j>=1){
+            //part 2 stars
+            j = 1;
+            while (j<=(i-1)*2){
+                System.out.print("*");
+                j++;
+            }
+            //part 3 again numbers
+            j = n - i + 1;
+            while (j >= 1){
                 System.out.print(j);
                 j--;
             }
